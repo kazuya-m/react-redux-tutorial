177afae477e0a5be587a9de4617d16cb58b32ebf
kazuya-m <kazuya.matsuo5@gmail.com>
2020-06-05 20:50:24 +0900

    get events

--- src/components/Events_index.js
+++ src/components/Events_index.js
@@ -1,40 +1,51 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { increment, decrement, reset, showYourName } from '../actions';
+import _ from 'lodash';
+
+import { readEvents } from '../actions';
 
 class EventsIndex extends Component {
+  componentDidMount() {
+    this.props.readEvents();
+  }
+
+  renderEvents() {
+    return _.map(this.props.events, (event, ind) => (
+      <tr key={ind}>
+        <td>{event.id}</td>
+        <td>{event.title}</td>
+        <td>{event.body}</td>
+      </tr>
+    ))
+  }
+
   render() {
-    const props = this.props;
-    const getInput = e => {
-      props.showYourName(e.target.value);
-    }
+    const props = this.props; 
     
     return (
       <React.Fragment>
-        <h3>Now : {props.value}</h3>
-        <button onClick={props.increment}>+1</button> 
-        <button onClick={props.decrement}>-1</button>
-        <button onClick={props.reset}>RESET!</button>
-        <h3>{props.yourname}</h3>
-        <input id='input' onChange={ e => {getInput(e)} }></input>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Body</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.renderEvents()}
+            {console.log(props.events)}
+          </tbody>
+        </table>
       </React.Fragment>
     );
   }
 }
 
 const mapStateToProps = state => {
-  return { value: state.count.value,
-           yourname: state.getname.yourname
-          }
+  return { events: state.events }
 }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    increment: () => dispatch(increment()),
-    decrement: () => dispatch(decrement()),
-    reset: () => dispatch(reset()),
-    showYourName: inputname => dispatch(showYourName(inputname))
-  }
-}
+const mapDispatchToProps = ({readEvents}); 
 
 export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex);
--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import reducer from './reducers';
-import App from './components/App';
+import EventsIndex from './components/Events_index.js';
 import * as serviceWorker from './serviceWorker';
+import thunk from 'redux-thunk';
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 
 
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+      <EventsIndex />
   </Provider>,
   document.getElementById('root')
 );
